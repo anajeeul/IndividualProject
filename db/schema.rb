ee031e0d4f00f11a6095dc6e29560e1a68e771b2
anajeeul <anajeeul@uccs.edu>
2021-05-07 20:26:22 +0000

    Migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 2021_05_02_181316) do
     t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
